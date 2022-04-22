8012b49abc1e1999f31a53ba56786c545713a78d
AbyxDev <ken@abyx.dev>
2022-04-22 01:09:16 -0400

    proper file logging

--- main.py
+++ main.py
@@ -25,6 +25,10 @@ SUBREDDITS = '+'.join([
     'vanaffy', # I mod this
 ])
 DEBUG = '-v' in sys.argv
+if DEBUG:
+    log = open('main.log', 'a')
+else:
+    log = sys.stdout
 
 reddit = praw.Reddit(
     client_id=client_id,
@@ -32,7 +36,8 @@ reddit = praw.Reddit(
     refresh_token=refresh_token,
     user_agent='Amongus spoilerer by u/Kenny2reddit')
 
-print('\n=== Logged in on', time.strftime('%Y-%m-%d %H:%M:%S'), '===\n', flush=True)
+print('\n=== Logged in on', time.strftime('%Y-%m-%d %H:%M:%S'), '===\n',
+      file=log, flush=True)
 
 OLD_TIME = 24 * 60 * 60 # one day
 TEXTUAL = re.compile(r'''^[-a-zA-Z0-9_'"\u2018-\u201f \u00a0\n?!:;.,()&$+=]+$''')
@@ -49,40 +54,40 @@ seen: dict[str, int] = {}
 for comment in reddit.subreddit(SUBREDDITS).stream.comments(skip_existing=True):
     # don't maintain cache older than a day, those get ignored anyway
     seen = {k: v for k, v in seen.items() if v > time.time() - OLD_TIME}
-    if DEBUG: print('Considering:', comment.permalink)
+    if DEBUG: print('Considering:', comment.permalink, file=log)
     comment: Comment
     if comment.author.name == me.name:
         # comment done by me
-        if DEBUG: print(' Comment is mine')
+        if DEBUG: print(' Comment is mine', file=log)
         continue
     if not comment.is_root:
         # only reply to top-level comments (increase visibility)
-        if DEBUG: print(' Comment not top-level')
+        if DEBUG: print(' Comment not top-level', file=log)
         continue
     if comment.submission.created_utc < time.time() - OLD_TIME:
         # don't reply to day-old posts
-        if DEBUG: print(' Post too old')
+        if DEBUG: print(' Post too old', file=log)
         continue
     if comment.submission.id in seen:
         # only comment once per submission
-        if DEBUG: print(' Post already commented on')
+        if DEBUG: print(' Post already commented on', file=log)
         continue
     if not TEXTUAL.match(comment.body):
         # markup and stuff that might break the spoiler
-        if DEBUG: print(' Comment too complicated')
+        if DEBUG: print(' Comment too complicated', file=log)
         continue
     amongusified = amongusify_censored(comment.body)
     # if the text is unchanged or completely spoiled, it wasn't amongusified
     if amongusified in {comment.body, reddit_spoiler(comment.body)}:
         # so don't just echo the comment
-        if DEBUG: print(' Comment not amogusified')
+        if DEBUG: print(' Comment not amogusified', file=log)
         continue
-    print('Replying to comment:', comment.permalink)
-    print('', amongusified)
+    print('Replying to comment:', comment.permalink, file=log)
+    print('', amongusified, file=log)
     try:
         comment.reply(amongusified + FOOTER)
     except PRAWException:
-        print(' Error replying:', flush=True)
+        print(' Error replying:', file=log, flush=True)
         traceback.print_exc()
     finally:
         seen[comment.submission.id] = comment.submission.created_utc
